897707140ff75a8fb79bdc6c70a7dcf1e8385c99
ethan dibble <edibble@pdx.edu>
2025-10-26 22:06:50 -0700

    chapter 12

---
+++ ch12/12.2.hs
@@ -0,0 +1,37 @@
+-- Exercise 12.2: Complete the following instance declaration to make the
+-- partially-applied function type (a ->) into a functor:
+
+instance Functor ((->) a) where
+  -- fmap :: (b -> c) -> (a -> b) -> (a -> c)
+  fmap = (.)
+
+{-
+when f = (->) a
+then f b = (->) a b = a -> b
+and  f c = (->) a c = a -> c
+
+Functor laws
+
+fmap id = \g -> id . g
+        = \g -> (\a -> id (g a))
+        = \g -> (\a -> g a)
+        = \g -> g
+        = id
+
+(g . h) :: b -> c
+a :: a -> b
+
+fmap (g . h) = \a -> (g . h) . a
+             = \a -> \b -> (g . h) (a b)
+             = \a -> \b -> g (h (a b))
+
+g :: b -> c
+h :: b -> c
+a :: a -> b
+
+fmap g . fmap h = (.) g . (.) h
+                = (.) g ((.) h)
+                = \a -> (.) g (h . a)
+                = \a -> g . h . a
+                = \a -> \b -> g (h (a b))
+-}
